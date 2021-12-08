0798ccfd2765e55e24c07eefcee46bd7ecb452de
Igor Serafim <igorserafim2015@hotmail.com>
2021-12-08 13:59:03 -0300

    Atualização do projeto

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+// Menu mobile
+const btnMenu = document.querySelector('.header .btn-menu')
+
+function toggleBtn() {
+  const imgs = this.querySelectorAll('img')
+  imgs.forEach(item => {
+    item.classList.toggle('btn-show')
+  })
+  
+  this.nextElementSibling.classList.toggle('menu-show')
+}
+btnMenu.addEventListener('click', toggleBtn)
+
+//===
+const navLink = document.querySelectorAll('.header .header-menu a')
+
+function removeMenu() {
+  const headerMenu = document.querySelector('.header-menu')
+  headerMenu.classList.remove('menu-show')
+  
+  const btnMenu = document.querySelectorAll('.header .btn-menu img')
+  btnMenu.forEach(item => {
+    item.classList.toggle('btn-show')
+  })
+}
+
+navLink.forEach(link => {
+  link.addEventListener('click', removeMenu)
+})
+
+
+
+
 // Dropdown Faq
 const dtFaq = document.querySelectorAll('.faq-accordion dt')
 
